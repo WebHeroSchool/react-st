414eb74710febc45b5195a85ef1117e320972d38
Татьяна Соснина <tatyana-sosnina@mail.ru>
2021-01-22 18:07:24 +0700

    React Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,104 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import ItemList from '../ItemList/ItemList';
-import InputItem from '../InputItem/InputItem';
-import Footer from '../Footer/Footer';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Card from '@material-ui/core/Card';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import Todo  from '../Todo/Todo';
+import About from '../About/About';
+import Contacts from '../Contacts/Contacts';
 import styles from './App.module.css';
 
-const App = () => {
-    const initialState = {
-        items: [
-            {
-               value: 'Написать новое приложение',
-               isDone: true,
-               id: 1
-            },
-            {
-               value: 'Дочитать книгу',
-               isDone: true,
-               id: 2
-             },
-            {
-               value: 'Закончить обучение',
-               isDone: false,
-               id: 3
-            }
-         ],
-         count: 3,
-         error: false
-    };
+const App = () =>
+ (<Router>
+  <div className={styles.wrap}>
+    <Card className={styles.sidebar}>
+        <MenuList>
+            <Link to='/' className={styles.link}><MenuItem className={styles.item}> Обо мне </MenuItem></Link>
+            <Link to='/todo' className={styles.link}><MenuItem> Дела </MenuItem></Link>
+            <Link to='/contacts' className={styles.link}><MenuItem> Контакты </MenuItem></Link>
+        </MenuList>
+    </Card>
 
-    const [items, setItems] = useState (initialState.items);
-  	const [count, setCount] = useState (initialState.count);
-  	const [error, setError] = useState (initialState.error);
-
-  	useEffect(() => {
-  		console.log('error update');
-  		return () => {
-  			console.clear()
-  		}
-  	}, [error]);
-
-  	useEffect(() => {
-  		console.log('update');
-  	});
-
-  	useEffect(() => {
-  		console.log('mount');
-  	}, []);
-
-	const onClickDone = id => {
-		const newItemList = items.map(item => {
-        const newItem = { ...item };
-
-        if (item.id === id) {
-          newItem.isDone = !item.isDone;
-        }
-
-        return newItem;
-      });
-
-    setItems(newItemList);
-    };
-
-  const onClickDelete = id => {
-    const newDeleteItem = items.filter(item => item.id !== id);
-		setItems(newDeleteItem);
-		setCount((count) => count - 1);
-	};
-
-   const onClickAdd = value => {
-        if(value !=='') {
-          setItems ([
-        ...items,
-        {
-
-                        value,
-                        isDone: false,
-                        id: count + 1
-                    }
-                  ]);
-    setCount((count) => count + 1);
-
-        } else {
-          setError (true);
-			}
-		};
-
-        return (
-            <div className={styles.wrap}>
-                 <h1 className={styles.title}>Важные дела!</h1>
-                 <div className ={styles.todosWrap}>
-                 <InputItem onClickAdd={onClickAdd}
-                  error={error}
-                  />
-                 <ItemList
-                  items={items}
-                  onClickDone={onClickDone}
-                  onClickDelete = {onClickDelete}
-                 />
-                 <Footer count={count} />
-           </div>
-     </div>);
-  }
+    <Card className={styles.content}>
+        <Route path='/' exact component={About} />
+        <Route path='/todo' component={Todo} />
+        <Route path='/contacts' component={Contacts} />
+    </Card>
+    </div>
+  </Router>
+);
 
 export default App;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,30 +3,42 @@ import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import styles from './Footer.module.css';
 
-const Footer = ({ count }) => (
-  <div className={styles.wrap}>
-         <Button size="medium" >
-         {count} items left
-        </Button>
-        <div className={styles.filter}>
-            <Button size="medium" >
-            <Button variant="outlined" color="primary">ALL</Button>
+class Footer extends React.Component {
+    buttons = [
+        {name: 'all', label: 'All'},
+        {name: 'active', label: 'Active'},
+        {name: 'completed', label: 'Completed'}
+    ];
+ render() {
+ const { count, onFilterChange} = this.props;
+    const buttons = this.buttons.map(({name, label}) => {
+        return(
+            <Button size="medium"
+                    key={name}
+                    onClick={() => onFilterChange(name)}>
+             {label}
             </Button>
-            <Button size="medium" >
-            <Button variant="outlined" color="secondary">Active</Button>
-            </Button>
-            <Button size="medium" >
-            <Button variant="outlined" disabled>Completed</Button>
-            </Button>
+          );
+})
+
+return(<div className={styles.wrap}>
+ <Button size="medium" >
+ {count} items left
+      </Button>
+      <div className={styles.filter} >
+          {buttons}
         </div>
-           <Button size="medium" >
-           Clear Completed
-        </Button>
-  </div>
+        <Button size="medium" >
+          Clear Completed
+          </Button>
+        </div>
 );
+}
+
+}
+
 
 Footer.propTypes = {
     count: PropTypes.number.isRequired
 };
-
-export default Footer;
+export default Footer; 
